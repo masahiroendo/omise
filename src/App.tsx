c7ca4d2c367f8fd9aec7463d583dee7a35b35165
matcha <massahiro.endo@gmail.com>
2022-12-02 17:57:29 +0100

    feat: tailwind init compl. + navbar in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { CiDark } from "react-icons/Ci";
+import { AiOutlineHome } from "react-icons/Ai";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <main className="bg-white px-10">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-2xl">Omise</h1>
+          <ul className="flex items-center">
+            <li>
+              <a href="#home">
+                <AiOutlineHome />
+              </a>
+            </li>
+            <li>
+              <button>
+                <CiDark className="text-3xl" />
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </main>
+    </div>
+  );
 }
 
 export default App;
